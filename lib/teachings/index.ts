faa9932151ae4dc27b0dcb18bcb4bedcc722b424
Cameron Bristol <cbristol28@yahoo.com>
2026-01-09 15:53:07 -0600

    Move data fetching to server, make function async

--- lib/teachings/index.ts
+++ lib/teachings/index.ts
@@ -1,2 +1,4 @@
 export { getCoverPhotoUrl } from "./getCoverPhotoUrl";
+export { getFeaturedTeaching } from "./getFeaturedTeaching";
+export { getRecentTeachings } from "./getRecentTeachings";
 export { type ScriptureReference, type Teaching, teachings } from "./teachings";
