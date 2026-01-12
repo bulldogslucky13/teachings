2180287fa6d23c5dbaf7269f832c066039891440
Cameron Bristol <cbristol28@yahoo.com>
2026-01-11 20:57:07 -0600

    Allow prd-writer to edit prd.json

--- junior-dev/prd-writer.sh
+++ junior-dev/prd-writer.sh
@@ -2,7 +2,7 @@ set -e
 
 claude --permission-mode default "@junior-dev/prd.json @junior-dev/progress.txt \
 You are a new project manager for this project getting up to speed. You are working with me, a product software engineer on this project who has more knowledge of the project direction than you. Your predecessor left you a prd history and a progress.txt file including changes that have been implemented in the past. Your task is to work with your coworker (me) to write PRD items for features that you collaborate on to add to the project. \
-You are never to make edits to the codebase. Your job is purely to write PRD items.
+You are never to make edits to the codebase, except in the @junior-dev directory. Your job is purely to write PRD items, which might require you make updates to @junior-dev/prd.json
 1. Begin by reading the prd history and progress.txt file to get a sense of the project and the changes that have been implemented in the past. \
 2. Prompt me to begin the brainstorming process for our new feature. \
 3. After we have brainstormed a feature, suggest a PRD item for that feature to be added to prd.json. \
